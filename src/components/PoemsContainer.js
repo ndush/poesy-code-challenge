74a7f2d17d7e68579ce1b270a0a05f42ca31fd07
damaris <mdamarisnduku@gmail.com>
2022-07-24 10:55:02 +0300

    install npm, display poems

--- src/components/PoemsContainer.js
+++ src/components/PoemsContainer.js
@@ -1,12 +1,21 @@
 import React from "react";
 import Poem from "./Poem";
 
-function PoemsContainer() {
+function PoemsContainer({poems}) {
   return (
     <div className="poems-container">
-      {/* render a list of <Poem> components in here */}
+      {poems.map(poem=>{
+        return(
+        <Poem
+        key={poem.id}
+        title={poem.title}
+        content={poem.content} 
+        author={poem.author}  />
+        )
+      })}
+      
     </div>
-  );
+  )
 }
 
 export default PoemsContainer;
